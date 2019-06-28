ceb9bbd1329a4f2662f87521525f55b90a619bd3
hanliang001 <hanliang001@163.com>
2019-06-28 18:19:26 +0800

    Add content for login page

--- lib/login.dart
+++ lib/login.dart
@@ -1,1 +1,9 @@
-login widget
+import 'package:flutter/material.dart';
+
+class login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container();
+  }
+}
